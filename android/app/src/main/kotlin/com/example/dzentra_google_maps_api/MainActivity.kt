f7705ccdae632cfda47c6ff09523fd2fe9bcc0cd
Ezeevidence <eze.pleasant234@gmail.com>
2021-10-07 10:54:33 +0100

    add google maps api and destination marker

---
+++ android/app/src/main/kotlin/com/example/dzentra_google_maps_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dzentra_google_maps_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/map.dart
@@ -0,0 +1,71 @@
+cd import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Maps extends StatefulWidget {
+  const Maps({Key? key}) : super(key: key);
+
+  @override
+  _MapsState createState() => _MapsState();
+}
+
+class _MapsState extends State<Maps> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  LatLng _lastMapPosition = _center;
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  void _onAddMarker() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            markers: _markers,
+            onCameraMove: _onCameraMove,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 10.0,
+            ),
+          ),
+
+          Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  onPressed: _onAddMarker,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  child: Icon(Icons.add_location, size: 36.0),
+                ),
+              ),
+          ),
+        ],
+      )
+    );
+  }
+}
